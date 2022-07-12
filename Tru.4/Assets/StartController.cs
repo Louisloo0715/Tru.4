f0ffceb3652a092d5814c5cbecf1b87cd89a5c03
Louisloo0715 <80396227+sarah8312598@users.noreply.github.com>
2022-07-12 18:00:44 +0800

    Update StartController.cs

--- Tru.4/Assets/StartController.cs
+++ Tru.4/Assets/StartController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartController : MonoBehaviour
 {
@@ -8,6 +9,34 @@ public class StartController : MonoBehaviour
 
     private void Start()
     {
-        LeanTween.moveLocalY(CharacterImage, -555, 1f).setEase(LeanTweenType.pingPong).setLoopPingPong();
+        LeanTween.moveLocalY(CharacterImage, -480, 1f).setEase(LeanTweenType.pingPong).setLoopPingPong();
     }
+    
+    public void LoadLevle(int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            Debug.Log(progress);
+            yield return null;
+        }
+    }
+
+    public void showPanel(GameObject gameObject)
+    {
+        LeanTween.scale(gameObject, Vector3.one, .5f).setEase(LeanTweenType.easeInExpo);
+
+    }
+    public void closePanel(GameObject gameObject)
+    {
+        LeanTween.scale(gameObject, Vector3.zero, .5f).setEase(LeanTweenType.easeInExpo);
+    }
+
 }
